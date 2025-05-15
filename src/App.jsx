9d54f6ea5190235211d949999e81f92f41eb72e9
yash patil <patilyash445@gmail.com>
2025-05-15 14:42:03 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,6 @@ import StatsCircles from './AnimatedCircle'
 import './App.css'
 import CursorFollower from './CursorFollower'
 import Footer from './Footer'
-// import JoinUs from './JoinUs'
-// import LatestVedanta from './LatestNews'
-// import OurBusiness from './OurBusiness'
-// import Quote from './Quote'
-// import StatsCarousel from './StatsSlider'
-// import Sustainability from './Sustainability'
-// import Navbar from './Navbar'
-// import Carousel from './Carousel'
-// import GlobalPresence from './GlobalPresence'
 import TravelCardSlider from './TravelCardSlider'
 import TestimonialSlider from './TestimonialSlider'
 import BrandsLogo from './BrandsLogo'
@@ -21,17 +12,9 @@ function App() {
     <div >
       <CursorFollower />
       <TestimonialSlider />
-      <BrandsLogo/>
+      <BrandsLogo />
       <TravelCardSlider />
       <StatsCircles />
-      {/* <Navbar />
-      <Carousel />
-      <OurBusiness />
-      <StatsCarousel />
-      <Quote />
-      <GlobalPresence />
-      <JoinUs />
-      <LatestVedanta /> */}
       <Footer />
     </div>
   )
